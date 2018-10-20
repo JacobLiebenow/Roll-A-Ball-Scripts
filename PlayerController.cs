7fe2914d1bc5a6c7cfe62a43b27f9ea622337027
Jacob Liebenow <liebenow.jacob@gmail.com>
2018-10-20 18:58:09 -0400

    Initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+    //Initialization goes here.
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    //Game code goes here.  Update() calls before rendering a frame
+    /*private void Update()
+    {
+        
+    }*/
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    //Physics code goes here.  FixedUpdate() calls before applying physics calculations
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
